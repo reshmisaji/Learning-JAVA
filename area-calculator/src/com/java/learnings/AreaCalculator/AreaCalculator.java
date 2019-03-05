0e73606ac333d8aad3112d1481ebcd3a00ad1282
Reshmi R S <44020004+reshmisaji@users.noreply.github.com>
2019-03-05 22:40:52 +0530

    Program to find the area of a circle and rectangle using method overloading

---
+++ area-calculator/src/com/java/learnings/AreaCalculator/AreaCalculator.java
@@ -0,0 +1,22 @@
+package com.java.learnings.AreaCalculator;
+
+public class AreaCalculator {
+    public static void main(String[] args) {
+        System.out.println(area(8.5D));
+    }
+
+    public static double area(double radius){
+        if(radius <0){
+            return -1.0;
+        }
+        return Math.PI*radius*radius;
+    }
+
+    public static double area(double x,double y){
+        if(x <0 || y <0){
+            return -1.0;
+        }
+        double areaOfRectangle = x*y;
+        return areaOfRectangle;
+    }
+}
