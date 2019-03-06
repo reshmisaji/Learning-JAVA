93be2fa9038f5a9a5e3177182c4aaa457f0ead1c
Reshmi R S <44020004+reshmisaji@users.noreply.github.com>
2019-03-06 09:24:36 +0530

    Program to check the equality of two decimal values

--- decimal-comparator/src/com/java/learnings/DecimalComparator/DecimalComparator.java
+++ decimal-comparator/src/com/java/learnings/DecimalComparator/DecimalComparator.java
@@ -1,2 +1,15 @@
+package com.java.learnings.DecimalComparator;
+import java.util.*;
+
 public class DecimalComparator {
+    public static void main(String[] args) {
+        System.out.println(areEqualByThreeDecimalPlaces(1D, 2D));
+    }
+
+    public static boolean areEqualByThreeDecimalPlaces(double firstDecimal, double secondDecimal) {
+        int firstNumber = (int) (firstDecimal*1000) ;
+        int secondNumber = (int) (secondDecimal*1000D);
+
+        return firstNumber == secondNumber;
+    }
 }
