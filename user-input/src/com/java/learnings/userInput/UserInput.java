42546a1355169875cd898b146b61b3fb6cfce12a
Reshmi R S <44020004+reshmisaji@users.noreply.github.com>
2019-03-06 10:41:43 +0530

    Program for input calculator challenge

---
+++ user-input/src/com/java/learnings/userInput/UserInput.java
@@ -0,0 +1,18 @@
+package com.java.learnings.userInput;
+
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number:");
+
+        while(scanner.hasNext()){
+
+        }
+
+        String a = scanner.next();
+        System.out.println(a);
+    }
+}
