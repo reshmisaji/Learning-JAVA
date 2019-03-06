24fb833e844d0fd78453f05e38b9073f399db650
Reshmi R S <44020004+reshmisaji@users.noreply.github.com>
2019-03-06 09:39:18 +0530

    Program to calculate the sum of odd numbers in a range

---
+++ sum-odd/src/com/java/learnings/sumOdd/SumOddRange.java
@@ -0,0 +1,25 @@
+package com.java.learnings.sumOdd;
+
+public class SumOddRange {
+
+    public static boolean isOdd(int number) {
+        if (number > 0 && number % 2 == 1) {
+            return true;
+        }
+        return false;
+    }
+
+    public static int sumOdd(int start, int end) {
+        int sum = 0;
+        if (start > end || start <0 ||end <0) {
+            return -1;
+        }
+
+        for (int index = start; index <= end; index++) {
+            if (isOdd(index)) {
+                sum += index;
+            }
+        }
+        return sum;
+    }
+}
